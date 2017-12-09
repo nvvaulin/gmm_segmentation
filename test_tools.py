3503cc21cc73b9a37b7a74422b3103d469a054b8
Niolay <nvvaulin@gmail.com>
2017-12-09 18:21:34 +0300

    train and test simple fcn without batchnorm, ndim=12,gm_num=4

--- test_tools.py
+++ test_tools.py
@@ -71,25 +71,26 @@ def make_features(feature_fn,imgs):
         data[i] = tmp
     return data
 
-def make_gmms(shape):
+def make_gmms(shape,gm_num):
     gmms = []
     for i in range(shape[0]):
         for j in range(shape[1]):
             gmms.append(mixture.GaussianMixture(covariance_type='diag',
-                               n_components=cfg.gm_num,
+                               n_components=gm_num,
                                max_iter=1000,
                                warm_start=True))
     return gmms
-    
+
     
 def fit_gmms(features,gmms,masks = None):
     for i in range(features.shape[1]):
         for j in range(features.shape[2]):
             f = features[:,i,j]
             if not (masks is None):
-                if(len(f[masks[:,i,j] < 30]) > cfg.gm_num*3):
+                gmm = gmms[i*features.shape[2]+j]
+                if(len(f[masks[:,i,j] < 30]) > gmm.n_components*3):
                     f = f[masks[:,i,j] < 30]
-            gmms[i*features.shape[2]+j].fit(f)
+            gmm.fit(f)
 
 def predict_pixelwise(features,gmms,predict_fn):
     res = np.zeros_like(features[:,:,:,0])
